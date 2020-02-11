be4ed104632521fad7109f72a9b32d949286c606
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-11 22:38:12 +0530

    Added the changes in jvascript files

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ var server = app.listen(3000 , () => {
 var io = socket(server)
 
 io.on('connection',function(socket) {
-    console.log("made socket connection")
+    console.log("made socket connection",socket.id)
 })
