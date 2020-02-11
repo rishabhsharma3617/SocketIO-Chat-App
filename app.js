f6b8f89ab1fbdbd8da3f89a9434eb472ae380761
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-11 22:27:52 +0530

    Added the javascrip file

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const  express = require('express')
 const app = express()
+const socket = require('socket.io')
 
 //Static code
 app.use(express.static('public'))
 
 
 
-app.listen(3000 , () => {
+var server = app.listen(3000 , () => {
     console.log("Listening to port 3000 , Server started")
+})
+
+//socket function takes the server name as the parameter to know on which server wh actually have to setu our socket.io
+var io = socket(server)
+
+io.on('connection',function(socket) {
+    console.log("made socket connection")
 })
