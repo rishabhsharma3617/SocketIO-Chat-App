131ce263b34ba9e709f7363e4d39b5be0dc2ace6
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-02-11 23:04:16 +0530

    Added the broadcasting

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message')
 var handle = document.getElementById('handle')
 var btn = document.getElementById('send')
 var output = document.getElementById('output')
+var feedback = document.getElementById('feedback')
 //Emit Events 
  btn.addEventListener('click',function() { 
      socket.emit('chat',{ //This object data is being sent to the server whenever send is clicked
@@ -14,7 +15,18 @@ var output = document.getElementById('output')
      })
  })
 
+ message.addEventListener('keypress',function(){
+     socket.emit('typing',handle.value)
+ })
+
+
+
  //Listen for events
  socket.on('chat',function(data){
+     feedback.innerHTML = ''
      output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+ })
+
+ socket.on('typing',function(data){
+     feedback.innerHTML = '<p><em>' + data + ' is typing the message' + '</em></p>'
  })
